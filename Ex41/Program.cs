fc22e3beab95f1bd5ab69825c4eeb5265539bd77
anna <a1b2v3@mail.ru>
2022-12-20 23:12:10 +0500

    add ex41

---
+++ Ex41/Program.cs
@@ -0,0 +1,55 @@
+﻿ int[] CreateArray(int count)
+ {
+    return new int[count];
+ }
+
+ // Метод заполнения массива
+ 
+ 
+  void Fill(int[]array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+       // Next() - заполнение случайными неотрицательными числами
+        array[index] = int.Parse(Console.ReadLine());
+        
+        index ++; 
+    }
+} 
+
+
+
+// // Печать массива
+  string Print(int[]array)
+{
+    return String.Join(' ', array);
+} 
+// // Определение количества  чисел, больше 0 в массиве
+    int count(int[]array)
+   {
+   int count = 0;
+   int length = array.Length;
+   
+   for (int i = 0; i < length; i++)
+   {
+     if (array[i]> 0)
+     count = count+1;
+     
+   }
+     return count;
+   }
+
+//string? M_str = Console.ReadLine();
+//int M = Convert.ToInt32(M_str);
+int M = int.Parse(Console.ReadLine());
+
+
+        int[]array = CreateArray(M);
+        Fill(array);
+        string array_text = Print(array);
+        int numbercount = count(array);
+        Console.WriteLine(array_text);
+        Console.WriteLine(numbercount);
+        
